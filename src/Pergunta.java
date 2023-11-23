4504e86e16bf3aa865e37829a7919c41579b9a6b
ZePedross <ZePedross@users.noreply.github.com>
2023-11-23 15:33:40 +0000

    Initial commit

---
+++ src/Pergunta.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Pergunta {
+    protected String pergunta;
+    protected String correta;
+    ArrayList<String> opcoes = new ArrayList<>();
+
+    public Pergunta(String per){
+
+    }
+}
